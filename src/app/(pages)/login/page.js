d7e136c46971c77558baae6d9bfe19e72d77d1d8
Sanjeev-Kumar08 <bhardwajsanjeev98055@gmail.com>
2025-01-22 12:29:14 +0530

    'minor changes'

--- src/app/(pages)/login/page.js
+++ src/app/(pages)/login/page.js
@@ -230,7 +230,6 @@ export default function LogInPage({ onSignUpClick, onForgotPasswordClick }) {
           {/* Image */}
           <div className="sm426:h-[1076px] sm426:w-[48%] sm375:w-[360px] sm350:w-[320px] w-[300px] h-[180px] sm426:order-2 sm426:mt-0 order-1 mt-4">
             <img
-              fetchPriority="high"
               alt="Art"
               src="/Art.svg"
               className="w-full h-full object-cover sm426:rounded-lg rounded-[20px]"
